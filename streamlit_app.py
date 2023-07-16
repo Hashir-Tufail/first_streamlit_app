4dd9a136c579c790ee07bededcaf0442f7502689
Hashir-Tufail <67013132+Hashir-Tufail@users.noreply.github.com>
2023-07-17 03:40:14 +0530

    added snowflke code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-import snowflake.connector
+
 streamlit.title('My Mom\'s New Healthy Diner')
 
 streamlit.header('Breakfast Favourites')
@@ -37,3 +37,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # display output in a table
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
+my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur=my_cnx.cursor()
+my_cur.execute(select current_user(),current_account(),current_region()")
+my_data_row=my_cur.fetchone()
+streamlit.text("Hello from Snowflake")
+streamlit.text(my_data_row)
